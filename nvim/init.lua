093bee11b5f1cd76944824bb0e275b979b13799b
paololeandro <dipaolosantos@gmail.com>
2022-09-16 14:04:14 -0300

    add some opt and keybindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,135 @@
+local g   = vim.g
+local o   = vim.o
+local opt = vim.opt
+local A   = vim.api
+
+-- cmd('syntax on')
+-- vim.api.nvim_command('filetype plugin indent on')
+
+o.termguicolors = true
+-- o.background = 'dark'
+
+-- Do not save when switching buffers
+-- o.hidden = true
+
+-- Decrease update time
+o.timeoutlen = 500
+o.updatetime = 200
+
+-- Number of screen lines to keep above and below the cursor
+o.scrolloff = 8
+
+-- Better editor UI
+o.number = true
+o.numberwidth = 2
+o.relativenumber = true
+-- o.signcolumn = 'yes'
+-- o.cursorline = true
+
+-- Better editing experience
+o.expandtab = true
+o.smarttab = true
+o.cindent = true
+o.autoindent = true
+o.wrap = true
+o.textwidth = 300
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = -1 -- If negative, shiftwidth value is used
+o.list = true
+o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂'
+-- o.listchars = 'eol:¬,space:·,lead: ,trail:·,nbsp:◇,tab:→-,extends:▸,precedes:◂,multispace:···⬝,leadmultispace:│   ,'
+-- o.formatoptions = 'qrn1'
+
+-- Makes neovim and host OS clipboard play nicely with each other
+o.clipboard = 'unnamedplus'
+
+-- Case insensitive searching UNLESS /C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+-- Undo and backup options
+o.backup = false
+o.writebackup = false
+o.undofile = true
+o.swapfile = false
+-- o.backupdir = '/tmp/'
+-- o.directory = '/tmp/'
+-- o.undodir = '/tmp/'
+
+-- Remember 50 items in commandline history
+o.history = 50
+
+-- Better buffer splitting
+o.splitright = true
+o.splitbelow = true
+
+-- Preserve view while jumping
+-- BUG This option causes an error!
+-- o.jumpoptions = 'view'
+
+-- BUG: this won't update the search count after pressing `n` or `N`
+-- When running macros and regexes on a large file, lazy redraw tells neovim/vim not to draw the screen
+-- o.lazyredraw = true
+
+-- Better folds (don't fold by default)
+-- o.foldmethod = 'indent'
+-- o.foldlevelstart = 99
+-- o.foldnestmax = 3
+-- o.foldminlines = 1
+--
+opt.mouse = "a"
+
+-- Map <leader> to space
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+-------------------------------------------------
+-- COLORSCHEMES
+-------------------------------------------------
+
+-- Uncomment just ONE of the following colorschemes!
+-- local ok, _ = pcall(vim.cmd, 'colorscheme base16-dracula')
+-- local ok, _ = pcall(vim.cmd, 'colorscheme base16-gruvbox-dark-medium')
+-- local ok, _ = pcall(vim.cmd, 'colorscheme base16-monokai')
+-- local ok, _ = pcall(vim.cmd, 'colorscheme base16-nord')
+-- local ok, _ = pcall(vim.cmd, 'colorscheme base16-oceanicnext')
+local ok, _ = pcall(vim.cmd, 'colorscheme base16-onedark')
+-- local ok, _ = pcall(vim.cmd, 'colorscheme palenight')
+-- local ok, _ = pcall(vim.cmd, 'colorscheme base16-solarized-dark')
+-- local ok, _ = pcall(vim.cmd, 'colorscheme base16-solarized-light')
+-- local ok, _ = pcall(vim.cmd, 'colorscheme base16-tomorrow-night')
+
+-- Highlight the region on yank
+A.nvim_create_autocmd('TextYankPost', {
+    group = num_au,
+    callback = function()
+        vim.highlight.on_yank({ higroup = 'Visual', timeout = 120 })
+    end,
+})
+
+-------------------------------------------------
+-- KEYBINDINGS
+-------------------------------------------------
+
+local function map(m, k, v)
+   vim.keymap.set(m, k, v, { silent = true })
+end
+
+-- Mimic shell movements
+map('i', '<C-E>', '<ESC>A')
+map('i', '<C-A>', '<ESC>I')
+
+-- Load recent sessions
+map('n', '<leader>sl', '<CMD>SessionLoad<CR>')
+
+-- Keybindings for telescope
+map('n', '<leader>fr', '<CMD>Telescope oldfiles<CR>')
+map('n', '<leader>ff', '<CMD>Telescope find_files<CR>')
+map('n', '<leader>fb', '<CMD>Telescope file_browser<CR>')
+map('n', '<leader>fw', '<CMD>Telescope live_grep<CR>')
+map('n', '<leader>ht', '<CMD>Telescope colorscheme<CR>')
+
 -------------------------------------------------
 -- DASHBOARD
 -------------------------------------------------
