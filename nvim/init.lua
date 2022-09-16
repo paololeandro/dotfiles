9fff2fbe5f65b8c39950eb97602affb13826c7a7
paololeandro <dipaolosantos@gmail.com>
2022-09-16 13:52:18 -0300

    add telescope plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -64,4 +64,50 @@ return require('packer').startup(function()
 
   -- Dashboard is a nice start screen for nvim
   use 'glepnir/dashboard-nvim'
+
+  -- Telescope and related plugins --
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { "nvim-telescope/telescope-file-browser.nvim",
+        config = function()
+        require("telescope").setup {
+          extensions = {
+            file_browser = {
+              theme = "ivy",
+              -- disables netrw and use telescope-file-browser in its place
+              hijack_netrw = true,
+              mappings = {
+                ["i"] = {
+                  -- your custom insert mode mappings
+                },
+                ["n"] = {
+                  -- your custom normal mode mappings
+                },
+              },
+            },
+          },
+        }
+        end
+  }
+  -- To get telescope-file-browser loaded and working with telescope,
+  -- you need to call load_extension, somewhere after setup function:
+  require("telescope").load_extension "file_browser"
+
+  -- Treesitter --
+  use {'nvim-treesitter/nvim-treesitter',
+       config = function()
+          require'nvim-treesitter.configs'.setup {
+          -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+          -- highlighting will fallback to default Vim syntax highlighting
+            highlight = {
+               enable = true,
+               additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+               },
+            ensure_installed = {'org'}, -- Or run :TSUpdate org
+            }
+       end
+  }
+
 end)
