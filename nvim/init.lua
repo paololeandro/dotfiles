51c2993ff34774b7fe74de38fb051c81fc8e6954
paololeandro <dipaolosantos@gmail.com>
2022-09-16 13:42:49 -0300

    add neovim lua configuration file

---
+++ nvim/init.lua
@@ -0,0 +1,32 @@
+-------------------------------------------------
+-- DASHBOARD
+-------------------------------------------------
+
+local db = require('dashboard')
+local home = os.getenv('HOME')
+
+db.default_banner = {
+  '',
+  '',
+  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+  '',
+  ' [ TIP: To exit Neovim, just power off your computer. ] ',
+  '',
+}
+
+-------------------------------------------------
+-- PLUGINS
+-------------------------------------------------
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Dashboard is a nice start screen for nvim
+  use 'glepnir/dashboard-nvim'
+end)
