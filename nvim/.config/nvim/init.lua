e8470ba2159eea670f7cfda6f727104d88d0a2ef
paololeandro <dipaolosantos@gmail.com>
2026-04-24 12:55:22 -0300

    change line numbers color

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,8 @@
 vim.wo.relativenumber = true
 
 vim.api.nvim_set_hl(0, "Normal", { bg="#242424" })
+
+vim.api.nvim_set_hl(0, 'LineNr', { fg = "#f4bf75"})
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = "#666666"})
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = "#f4bf75"})
+
